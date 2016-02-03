bfe0a4dca5cd3a417a19c04814050929d8a0cf6e
Simon Jinaphant <SimonJinaphant@users.noreply.github.com>
2016-02-03 08:28:39 -0800

    Inserted comments and doc

--- challenges/serializeBT.py
+++ challenges/serializeBT.py
@@ -10,22 +10,39 @@ class Node:
         self.index_lookup = None
 
     def preprocess_inorder(self, inorder):
+        """Build a look-up table to find the index of a value for the in-order list
+
+        :param inorder: in-order traversal list
+        :return: A dictionary with the in-order value as key and in-order index as values
+        """
         return {num: i for i, num in enumerate(inorder)}
 
     def deserialize(self, preorder, inorder):
+        """Deserialize a binary tree given its pre-order and in-order traversal
+
+        :param preorder: pre-order traversal list
+        :param inorder: in-order traversal list
+        :return: The corresponding binary tree which was serialized
+        """
         self.index_lookup = self.preprocess_inorder(inorder)
-        print self.index_lookup
         return self._deserialize(preorder, inorder)
 
-    def _deserialize(self, preorder, inorder):
+    def _deserialize(self, preorder, inorder)
+        """Inner recursive implementation of the deserialization
+
+        :param preorder: pre-order traversal list
+        :param inorder: in-order traversal list
+        :return: The corresponding binary tree which was serialized
+        """
         if len(preorder) == 0 or len(inorder) == 0:
             return None
 
         root = Node()
         root.value = preorder[0]
         root_index = self.index_lookup[root.value]
-        print "Preorder: {0}, Inorder: {1}".format(preorder, inorder)
-        print "Root value: {0}, Index in inorder: {1}".format(root.value,root_index)
+
+        #print "Preorder: {0}, Inorder: {1}".format
+        #print "Root value: {0}, Index in inorder: {1}".format(root.value,root_index)
 
         root.left = self._deserialize(preorder[1:root_index+1], inorder[:root_index])
         root.right = self._deserialize(preorder[root_index+1:], inorder[root_index+1:])
@@ -33,6 +50,9 @@ class Node:
         return root
 
     def traverse_by_level(self):
+        """Traverse the binary level by level,
+        printing out all non-None nodes
+        """
         q = Queue()
         q.put_nowait(self)
 
