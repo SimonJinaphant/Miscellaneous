340c97ae65578c59f2fc6a42a73e0939640a80ab
Simon Jinaphant <SimonJinaphant@users.noreply.github.com>
2016-02-07 09:02:15 -0800

    Refactor to use unittest for RabinKarp

--- challenges/rabin_karp.py
+++ challenges/rabin_karp.py
@@ -1,10 +1,13 @@
+import unittest
+
+
 def substring(text, pattern):
     """
     Determines the first index of the given pattern in text
 
     :param text: The string to be searched on
     :param pattern: The string to match
-    :return: The first index of a matched pattern, otherwise -1 if pattern was not found in text
+    :return: The first index of a matched pattern, otherwise None if pattern was not found in text
     """
     prime = 101
 
@@ -51,10 +54,20 @@ def substring(text, pattern):
         hashed_subtext = recompute_hash(i, i+m, hashed_subtext)
     
     #Still need to check the last recomputed hash
-    return n - m if (hashed_pattern == hashed_subtext and pattern == text[n-m:]) else -1
+    return n - m if (hashed_pattern == hashed_subtext and pattern == text[n-m:]) else None
 
-print substring("abedabc", "abc")
-print substring("carpet", "arp")
-print substring("noodleandneedleinastack","needle")
-print substring("java", "ada")
-print substring("snowwy","snow")
+
+class TestRabinKarp(unittest.TestCase):
+
+    def test_ends(self):
+        self.assertEqual(substring("abedabc","abc"), 4)
+        self.assertEqual(substring("carpet", "car"), 0)
+
+    def test_normal(self):
+        self.assertEqual(substring("aneedleinahaystack", "needle"), 1)
+
+    def test_absent(self):
+        self.assertEqual(substring("java", "ada"), None)
+
+if __name__ == "__main__":
+    TestRabinKarp.main()
