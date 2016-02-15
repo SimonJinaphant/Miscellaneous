368bfe8f886fae585d82d2e5ac89bf91e36f0597
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-02-14 20:54:03 -0800

    No division implementation

--- challenges/product_array.py
+++ challenges/product_array.py
@@ -23,9 +23,30 @@ def product_array(numbers):
     return results
 
 
+def product_array_no_div(numbers):
+    results = []
+    product = 1
+
+    for num in numbers:
+        results.append(product)
+        product *= num
+
+    product = 1
+    for i in xrange(len(numbers)-1, -1, -1):
+        results[i] *= product
+        product *= numbers[i]
+
+    return results
+
+
 class TestProductArray(unittest.TestCase):
     def test_normal(self):
         self.assertEquals(product_array([2, 1, 3, 4, 1, 2]), [24, 48, 16, 12, 48, 24])
+        self.assertEqual(product_array([10, 3, 5, 6, 2]), [180, 600, 360, 300, 900])
+
+    def test_no_div_normal(self):
+        self.assertEquals(product_array_no_div([2, 1, 3, 4, 1, 2]), [24, 48, 16, 12, 48, 24])
+        self.assertEqual(product_array_no_div([10, 3, 5, 6, 2]), [180, 600, 360, 300, 900])
 
 if __name__ == "__main__":
     unittest.main
