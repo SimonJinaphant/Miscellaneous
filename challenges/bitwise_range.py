ad9caa69d70c64760110bdb78bcc320d55fcd533
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2016-03-04 16:30:12 -0800

    Updated bug fix with bitwise range

--- challenges/bitwise_range.py
+++ challenges/bitwise_range.py
@@ -4,33 +4,37 @@ def generate_bitwise_ones(n):
     :param n: The amount of '1's to generate
     :return: A binary of only '1's that is of size n
     """
+
     return (1 << n) - 1
 
 
-def generate_bitwise_range(n, m):
-    """Generate a binary number with '1's between the bit index of n and m
-    Precondition: n >= m
+def generate_bitwise_range(upper, lower):
+    """Generate a binary number with '1's between the bit index range
+    Precondition: upper > lower
 
-    :param n: The upper bit index (inclusive)
-    :param m: The lower bit index (inclusive)
-    :return: A binary with (n - m) '1's shifted by m bits
+    :param upper: The upper bit index (inclusive)
+    :param lower: The lower bit index (inclusive)
+    :return: A binary with (:upper - :lower + 1) amount of '1's shifted by :lower bits
     """
-    return generate_bitwise_ones(n) & ~generate_bitwise_ones(m)
+
+    return generate_bitwise_ones(upper+1) & ~generate_bitwise_ones(lower)
 
 
-def set_bits(n, m, i, j):
-    """Given two binary numbers, set the first binary's bits between i and j to be m
-    Precondition: m just have (j - i) digits and ||n|| > ||m||
+def set_bits(n, m, upper, lower):
+    """Given two binary numbers, set the first binary's bits between i and j to that of m
+    Precondition: ||n|| > ||m||
 
     :param n: The larger binary to be changed
     :param m: The smaller binary to be set
-    :param i: The lower bit index (inclusive)
-    :param j: The upper bit index (inclusive)
-    :return:
+    :param upper: The lower bit index (inclusive)
+    :param lower: The upper bit index (inclusive)
+    :return: n with its :upper and :lower bits (both inclusive) matching that of m
     """
-    n &= ~generate_bitwise_range(j+1, i)
-    m <<= i
+
+    n &= ~generate_bitwise_range(upper, lower)
+    m <<= lower
+
     return n | m
 
 
-print bin(set_bits(0b10000001000, 0b10101, 2, 6))
+print bin(set_bits(0b10000000000, 0b111, 3, 1))
